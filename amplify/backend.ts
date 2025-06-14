10b18a1708767027a09e4b08873b6a117fc4b82b
Shuvam Devkota <officialdevkota@gmail.com>
2025-06-14 18:31:44 +0545

    Censor detection, SNS not working

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,48 @@
 import { defineBackend } from '@aws-amplify/backend';
-import { auth } from './auth/resource.js';
-import { data } from './data/resource.js';
+import { auth } from './auth/resource';
+import { data } from './data/resource';
+import { contentModerator } from './functions/content-moderator/resource';
+import { Effect, PolicyStatement } from 'aws-cdk-lib/aws-iam';
+import { Topic } from 'aws-cdk-lib/aws-sns';
+import { EmailSubscription } from 'aws-cdk-lib/aws-sns-subscriptions';
 
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
+  contentModerator,
 });
+
+// Create SNS Topic
+const snsTopic = new Topic(backend.contentModerator.resources.lambda, 'ContentModerationTopic', {
+  topicName: 'content-moderation-alerts',
+  displayName: 'Content Moderation Alerts'
+});
+
+// Add email subscription
+snsTopic.addSubscription(new EmailSubscription('officialdevkota@gmail.com'));
+
+// Grant Lambda permission to publish to SNS
+backend.contentModerator.resources.lambda.addToRolePolicy(
+  new PolicyStatement({
+    effect: Effect.ALLOW,
+    actions: ['sns:Publish'],
+    resources: [snsTopic.topicArn],
+  })
+);
+
+// Add environment variable for SNS topic ARN
+backend.contentModerator.addEnvironment('SNS_TOPIC_ARN', snsTopic.topicArn);
+
+// Grant Lambda permission to read from DynamoDB (for manual invocation)
+const table = backend.data.resources.tables['Todo'];
+backend.contentModerator.resources.lambda.addToRolePolicy(
+  new PolicyStatement({
+    effect: Effect.ALLOW,
+    actions: [
+      'dynamodb:GetItem',
+      'dynamodb:Query',
+      'dynamodb:Scan'
+    ],
+    resources: [table.tableArn],
+  })
+);
